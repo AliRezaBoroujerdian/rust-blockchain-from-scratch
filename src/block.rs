2769ff451da84199a1cdda9074db5b6fceee335e
AliReza Boroujerdian <boroujerdian.alireza@gmail.com>
2024-04-26 01:09:25 +0330

    impl new_block

--- src/block.rs
+++ src/block.rs
@@ -1,6 +1,3 @@
-
-
-
 /// Block keeps block headers
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Block {
@@ -10,4 +7,26 @@ pub struct Block {
     hash: String,
     nonce: i32,
     height: i32,
+}
+
+impl Block {
+    pub fn new_block(
+        transactions: Vec<Transaction>,
+        prev_block_hash: String,
+        height: i32,
+    ) -> Result<Block> {
+        let timestamp = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)?
+            .as_millis();
+        let mut block = Block {
+            timestamp,
+            transactions,
+            prev_block_hash,
+            hash: String::new(),
+            nonce: 0,
+            height,
+        };
+        block.run_proof_of_work()?;
+        Ok(block)
+    }
 }
