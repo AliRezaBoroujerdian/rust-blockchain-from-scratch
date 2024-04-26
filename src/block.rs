ccc585ee61b1d76bcb61a62a33835763a7aeb2b9
AliReza Boroujerdian <boroujerdian.alireza@gmail.com>
2024-04-26 23:41:15 +0330

    Add run_proof_of_work

--- src/block.rs
+++ src/block.rs
@@ -29,4 +29,40 @@ impl Block {
         block.run_proof_of_work()?;
         Ok(block)
     }
+
+    /// Run performs a proof-of-work
+    fn run_proof_of_work(&mut self) -> Result<()> {
+        info!("Mining the block");
+        while !self.validate()? {
+            self.nonce += 1;
+        }
+        let data = self.prepare_hash_data()?;
+        let mut hasher = Sha256::new();
+        hasher.input(&data[..]);
+        self.hash = hasher.result_str();
+        Ok(())
+    }
+
+    /// HashTransactions returns a hash of the transactions in the block
+    fn hash_transactions(&self) -> Result<Vec<u8>> {
+        let mut transactions = Vec::new();
+        for tx in &self.transactions {
+            transactions.push(tx.hash()?.as_bytes().to_owned());
+        }
+        let tree = CBMT::<Vec<u8>, MergeVu8>::build_merkle_tree(&transactions);
+
+        Ok(tree.root())
+    }
+
+    fn prepare_hash_data(&self) -> Result<Vec<u8>> {
+        let content = (
+            self.prev_block_hash.clone(),
+            self.hash_transactions()?,
+            self.timestamp,
+            TARGET_HEXS,
+            self.nonce,
+        );
+        let bytes = serialize(&content)?;
+        Ok(bytes)
+    }
 }
